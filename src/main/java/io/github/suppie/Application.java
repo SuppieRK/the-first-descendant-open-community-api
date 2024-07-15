2c163cd29fdb2c48280b3d3514b9c91eb36cebc3
Roman Khlebnov <suppie.rk@gmail.com>
2024-07-15 18:15:18 +0300

    Updating CI scripts (#1)
    * Updating CI scripts
    
    * Updating CI scripts

--- src/main/java/io/github/suppie/Application.java
+++ src/main/java/io/github/suppie/Application.java
@@ -4,7 +4,8 @@ import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "The First Descendant Open Community API", version = "0.0.1-SNAPSHOT"))
+@OpenAPIDefinition(
+    info = @Info(title = "The First Descendant Open Community API", version = "0.0.1-SNAPSHOT"))
 public class Application {
   public static void main(String[] args) {
     Micronaut.build(args).eagerInitSingletons(true).banner(false).start();
